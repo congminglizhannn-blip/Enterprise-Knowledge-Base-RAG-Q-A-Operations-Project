b7d91ffe93faab9f6a7462d2d453f929043549e0
cleverli <lzh111byx@126.com>
2026-09-25 22:47:31 +0800

    历史数据上传

---
+++ rag_db.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict rHI6uZ1awcPPG0gY7wbijlekaTs9265XB9G5gY4nhFwLBD7zzOBiAbTzVaFBuF9
+
+-- Dumped from database version 18.6
+-- Dumped by pg_dump version 18.6
+
+-- Started on 2026-09-25 22:45:35
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2026-09-25 22:45:35
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict rHI6uZ1awcPPG0gY7wbijlekaTs9265XB9G5gY4nhFwLBD7zzOBiAbTzVaFBuF9
+
